b4d3c9450f920da8a2637a92255d10c8f53ed191
Busak Bence <busakbence@gmail.com>
2023-11-19 17:24:38 +0100

    Repository class created and completed

---
+++ SWZZPI_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace SWZZPI_HFT_2023241.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected LolDBContext ctx;
+        public Repository(LolDBContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);      
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract void Update(T item);
+    }
+}
