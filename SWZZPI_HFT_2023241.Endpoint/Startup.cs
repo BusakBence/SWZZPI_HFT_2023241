cf7955c50ca1e83cf56ada3a5a083294d63b0f04
Busak Bence <busakbence@gmail.com>
2023-12-02 23:58:33 +0100

    Modified the Startup

--- SWZZPI_HFT_2023241.Endpoint/Startup.cs
+++ SWZZPI_HFT_2023241.Endpoint/Startup.cs
@@ -1,8 +1,8 @@
-using Castle.Core.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -18,13 +18,14 @@ namespace SWZZPI_HFT_2023241.Endpoint
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; set; }
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<LolDBContext>();
