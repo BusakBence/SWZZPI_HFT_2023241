01fd76e0dc4dc85fda57baf09d273f50f60e9728
Busak Bence <busakbence@gmail.com>
2023-12-02 17:57:28 +0100

    Created the Update methods in Client

--- SWZZPI_HFT_2023241.Client/Program.cs
+++ SWZZPI_HFT_2023241.Client/Program.cs
@@ -101,6 +101,47 @@ namespace SWZZPI_HFT_2023241.Client
             }
         }
         #endregion
+        #region Updates
+        static void UpdateChampions(string champion)
+        {
+            if (champion == "Champion")
+            {
+                Console.Write("Enter the champion's id: ");
+                int oldId = int.Parse(Console.ReadLine());
+                Champions old = restService.Get<Champions>(oldId, "champion");
+                Console.Write($"Enter the champion's new name (old name: {old.Name}): ");
+                string name = Console.ReadLine();
+                old.Name = name;
+                restService.Put(old, "champion");
+            }
+        } 
+        static void UpdateRegions(string region)
+        {
+            if (region == "Region")
+            {
+                Console.Write("Enter the region's id: ");
+                int oldId = int.Parse(Console.ReadLine());
+                Regions old = restService.Get<Regions>(oldId, "champion");
+                Console.Write($"Enter the region's new name (old name: {old.Name}): ");
+                string name = Console.ReadLine();
+                old.Name = name;
+                restService.Put(old, "region");
+            }
+        }
+        static void UpdateAbilities(string ability)
+        {
+            if (ability == "Ability")
+            {
+                Console.Write("Enter the ability's id: ");
+                int oldId = int.Parse(Console.ReadLine());
+                Abilities old = restService.Get<Abilities>(oldId, "ability");
+                Console.Write($"Enter the ability's new name (old name: {old.Name}): ");
+                string name = Console.ReadLine();
+                old.Name = name;
+                restService.Put(old, "ability");
+            }
+        }
+#endregion
         static void Main(string[] args)
         {
             restService = new RestService("http://localhost:30487/");           
