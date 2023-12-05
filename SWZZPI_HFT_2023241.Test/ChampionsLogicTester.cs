23b898891f3606dd0ee2f0de27a6528fda579ca7
Busak Bence <busakbence@gmail.com>
2023-12-05 23:23:01 +0100

    Created the test for sixth non crud

--- SWZZPI_HFT_2023241.Test/ChampionsLogicTester.cs
+++ SWZZPI_HFT_2023241.Test/ChampionsLogicTester.cs
@@ -22,12 +22,12 @@ namespace SWZZPI_HFT_2023241.Test
             mockChampionsRepo = new Mock<IRepository<Champions>>();
             mockChampionsRepo.Setup(t => t.ReadAll()).Returns(new List<Champions>()
             {
-                
+
                 new Champions() { Id = 1, Name = "Janos", Gender = "Male", Species = "Human", Lane = "Middle", ReleaseYear = 2011, RegionsId = 3},
-                new Champions() { Id = 2, Name = "Daniel", Gender = "Male", Species = "Demon", Lane = "Bottom", ReleaseYear = 2009, RegionsId = 2},
+                new Champions() { Id = 2, Name = "Daniel", Gender = "Male", Species = "Demon", Lane = "Middle, Bottom", ReleaseYear = 2009, RegionsId = 2},
                 new Champions() { Id = 3, Name = "Rebeka", Gender = "Female", Species = "Yordle", Lane = "Top", ReleaseYear = 2017, RegionsId = 5},
                 new Champions() { Id = 4, Name = "Imre", Gender = "Male", Species = "Vastayan", Lane = "Support", ReleaseYear = 2021, RegionsId = 4},
-                new Champions() { Id = 5, Name = "Abel", Gender = "Male", Species = "God", Lane = "Jungle", ReleaseYear = 2015, RegionsId = 1},               
+                new Champions() { Id = 5, Name = "Abel", Gender = "Male", Species = "God", Lane = "Jungle", ReleaseYear = 2015, RegionsId = 1},
             }.AsQueryable());
             mockRegionsRepo = new Mock<IRepository<Regions>>();
             mockRegionsRepo.Setup(x => x.ReadAll()).Returns(new List<Regions>()
@@ -36,7 +36,7 @@ namespace SWZZPI_HFT_2023241.Test
                 new Regions() {Id = 2, Name = "Noxus", Location = "Dél", TechnologyLevel = "Magas", FormOfGovernment = "Köztársaság", Environment = "Kontinentális" },
                 new Regions() {Id = 3, Name = "Ionia", Location = "Észak", TechnologyLevel = "Magas", FormOfGovernment = "Köztársaság", Environment = "Kontinentális" },
                 new Regions() {Id = 4, Name = "Demacia", Location = "Nyugat", TechnologyLevel = "Közepes", FormOfGovernment = "Köztársaság", Environment = "Kontinentális" },
-                new Regions() {Id = 5, Name = "Freljord", Location = "Kelet", TechnologyLevel = "Közepes", FormOfGovernment = "Köztársaság", Environment = "Kontinentális" },                
+                new Regions() {Id = 5, Name = "Freljord", Location = "Kelet", TechnologyLevel = "Közepes", FormOfGovernment = "Köztársaság", Environment = "Kontinentális" },
             }.AsQueryable());
             mockAbilitiesRepo = new Mock<IRepository<Abilities>>();
             mockAbilitiesRepo.Setup(y => y.ReadAll()).Returns(new List<Abilities>()
@@ -46,7 +46,7 @@ namespace SWZZPI_HFT_2023241.Test
                 new Abilities() { Id = 3, Name = "Hukk", AbilityKey = 'R', ChampionId = 3 },
                 new Abilities() { Id = 4, Name = "Pukk", AbilityKey = 'E', ChampionId = 5 },
                 new Abilities() { Id = 5, Name = "Darum", AbilityKey = 'W', ChampionId = 4 },
-                
+
             }.AsQueryable());
             foreach (var champion in mockChampionsRepo.Object.ReadAll())
             {
@@ -70,17 +70,17 @@ namespace SWZZPI_HFT_2023241.Test
             }
             foreach (var champion in mockChampionsRepo.Object.ReadAll())
             {
-                foreach (var region  in mockRegionsRepo.Object.ReadAll())
+                foreach (var region in mockRegionsRepo.Object.ReadAll())
                 {
                     if (champion.RegionsId == region.Id)
                     {
                         champion.Region = region;
                     }
                 }
-            }   
+            }
             foreach (var ability in mockAbilitiesRepo.Object.ReadAll())
             {
-                foreach (var champion  in mockChampionsRepo.Object.ReadAll())
+                foreach (var champion in mockChampionsRepo.Object.ReadAll())
                 {
                     if (ability.ChampionId == champion.Id)
                     {
@@ -88,14 +88,14 @@ namespace SWZZPI_HFT_2023241.Test
                     }
                 }
             }
-            championsLogic = new ChampionsLogic(mockChampionsRepo.Object, mockRegionsRepo.Object, mockAbilitiesRepo.Object);          
+            championsLogic = new ChampionsLogic(mockChampionsRepo.Object, mockRegionsRepo.Object, mockAbilitiesRepo.Object);
         }
         [Test]
         public void GetShurimaChampionsBetween2012And2016Test()
         {
-             var expectedHeroes = new List<ShurimaChampions> { new ShurimaChampions() { Name = "Abel", Region = "Shurima", Year = 2015 } };
-             var actualHeroes = championsLogic.GetShurimaChampionsBetween2012And2016();           
-             CollectionAssert.AreEqual(expectedHeroes, actualHeroes);          
+            var expectedHeroes = new List<ShurimaChampions> { new ShurimaChampions() { Name = "Abel", Region = "Shurima", Year = 2015 } };
+            var actualHeroes = championsLogic.GetShurimaChampionsBetween2012And2016();
+            CollectionAssert.AreEqual(expectedHeroes, actualHeroes);
         }
         [Test]
         public void GetFemalesUltimatesTest()
@@ -114,7 +114,7 @@ namespace SWZZPI_HFT_2023241.Test
         [Test]
         public void GetDemacianAbilitiesTest()
         {
-            var expectedAbilities = new List<DemacianAbilities> { new DemacianAbilities() {  ChampionName = "Imre", Name = "Darum", Key = 'W', Region = "Demacia" } };
+            var expectedAbilities = new List<DemacianAbilities> { new DemacianAbilities() { ChampionName = "Imre", Name = "Darum", Key = 'W', Region = "Demacia" } };
             var actualAbilities = championsLogic.GetDemacianAbilities();
             CollectionAssert.AreEqual(expectedAbilities, actualAbilities);
         }
@@ -124,7 +124,21 @@ namespace SWZZPI_HFT_2023241.Test
             var expectedPAbilities = new List<DChampionsPAbilities> { new DChampionsPAbilities() { Name = "Daniel", Key = 'P', KeyName = "Kepesseg" } };
             var actualPAbilities = championsLogic.GetDChampionsPAbilities();
             CollectionAssert.AreEqual(expectedPAbilities, actualPAbilities);
-        }   
+        }
+        [Test]
+        public void GetMoreThanTwoLanesTest()
+        {
+            var expectedLanes = new List<MoreThanTwoLanes> { new MoreThanTwoLanes() { Name = "Daniel", Lane = "Middle, Bottom", Region = "Noxus" } };
+            var actualLanes = championsLogic.GetMoreThanTwoLanes();
+            CollectionAssert.AreEqual(expectedLanes, actualLanes);
+        }
+        [Test]
+        public void GetAllIoniansTest2()
+        {
+            var expected = 1;
+            var actual = championsLogic.GetAllIonianChampions().Count;
+            Assert.AreEqual(expected, actual);
+        }
         [Test]
         public void CreateNewChampionCorrectlyTest()
         {
@@ -141,14 +155,14 @@ namespace SWZZPI_HFT_2023241.Test
                 championsLogic.Create(newChampion);
             }
             catch (Exception)
-            {                
-            }            
+            {
+            }
             mockChampionsRepo.Verify(c => c.Create(newChampion), Times.Never);
         }
         [Test]
         public void CreateNewChampionCorrectlyTest2()
         {
-            var newChampion = new Champions() { Species = "Vámpír"};
+            var newChampion = new Champions() { Species = "Vámpír" };
             try
             {
                 championsLogic.Create(newChampion);
@@ -157,13 +171,6 @@ namespace SWZZPI_HFT_2023241.Test
             {
             }
             mockChampionsRepo.Verify(c => c.Create(newChampion), Times.Never);
-        }
-        [Test]
-        public void GetAllIoniansTest2()
-        {
-            var expected = 1;
-            var actual = championsLogic.GetAllIonianChampions().Count;
-            Assert.AreEqual(expected, actual);
-        }
+        }      
     }   
 }
