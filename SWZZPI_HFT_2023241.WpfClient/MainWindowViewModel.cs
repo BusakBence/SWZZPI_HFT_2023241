2d75964e96f5126a6721e87ef6a32aff61df985b
Busak Bence <busakbence@gmail.com>
2024-05-06 18:30:35 +0200

    Created the commands

--- SWZZPI_HFT_2023241.WpfClient/MainWindowViewModel.cs
+++ SWZZPI_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SWZZPI_HFT_2023241.WpfClient
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
+        static RestService restService;
+        public ICommand ChampionsWindowCommand;
+        public ICommand RegionsWindowCommand;
+        public ICommand AbilitiesWindowCommand;
+        public ICommand NonCrudsWindowCommand;
+        public MainWindowViewModel()
+        {
+            restService = new RestService("http://localhost:30487/", "swagger");
 
+        }
     }
 }
