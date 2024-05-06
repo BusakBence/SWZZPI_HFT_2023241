457c9093e127f537b0decd6e7ccf3fad57f1a91d
Busak Bence <busakbence@gmail.com>
2024-05-06 18:21:59 +0200

    Classes created

---
+++ SWZZPI_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWZZPI_HFT_2023241.WpfClient
+{
+    class MainWindowViewModel
+    {
+
+    }
+}
