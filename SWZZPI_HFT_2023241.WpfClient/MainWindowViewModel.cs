7d8f97de6803885059bcbc703bb1c31486933f65
Busak Bence <busakbence@gmail.com>
2024-05-06 18:32:54 +0200

    Commands

--- SWZZPI_HFT_2023241.WpfClient/MainWindowViewModel.cs
+++ SWZZPI_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,22 @@ namespace SWZZPI_HFT_2023241.WpfClient
         public MainWindowViewModel()
         {
             restService = new RestService("http://localhost:30487/", "swagger");
+            ChampionsWindowCommand = new RelayCommand(() =>
+            {
 
+            });
+            RegionsWindowCommand = new RelayCommand(() =>
+            {
+
+            });
+            AbilitiesWindowCommand = new RelayCommand(() =>
+            {
+
+            });
+            NonCrudsWindowCommand = new RelayCommand(() =>
+            {
+
+            });
         }
     }
 }
